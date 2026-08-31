3b3163d6d60b7b5bdebbc26c9206280e738b3a47
anonympins <anonympins@gmail.com>
2026-08-31 05:29:16 +0200

    fixed wasm build

--- src/cpp/utils.cpp
+++ src/cpp/utils.cpp
@@ -7,12 +7,12 @@ uint64_t cyrb53(const std::string& str, uint32_t seed) {
     int32_t h2 = 0x41c6ce57 ^ seed;
 
     for (char ch : str) {
-        h1 = imul(h1 ^ ch, 2654435761);
+        h1 = imul(h1 ^ ch, -1640531535); // 2654435761 en int32
         h2 = imul(h2 ^ ch, 1597334677);
     }
 
-    h1 = imul(h1 ^ (h1 >> 16), 2246822507) ^ imul(h2 ^ (h2 >> 13), 3266489909);
-    h2 = imul(h2 ^ (h2 >> 16), 2246822507) ^ imul(h1 ^ (h1 >> 13), 3266489909);
+    h1 = imul(h1 ^ (h1 >> 16), -2048144789) ^ imul(h2 ^ (h2 >> 13), -1028477387); // 2246822507 et 3266489909 en int32
+    h2 = imul(h2 ^ (h2 >> 16), -2048144789) ^ imul(h1 ^ (h1 >> 13), -1028477387);
 
     // Combine les deux hash 32-bit en un hash 64-bit (similaire à la version JS)
     // Note : La conversion en uint64_t est importante pour correspondre au résultat non signé de JS.
