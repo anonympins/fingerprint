84a16a035fc5fcea474c29e19befae7bc0b6c09b
anonympins <anonympins@gmail.com>
2026-09-14 18:38:57 +0200

    Update fingerprint.js

--- src/js/fingerprint.js
+++ src/js/fingerprint.js
@@ -124,7 +124,7 @@ const yandexEntries = loadBotWhitelist('yandex.json', [
 ]);
 
 function generateSessionMapping() {
-    const randomStr = (len = 6) => Array.from({ length: len }, () => String.fromCharCode(97 + crypto.randomBytes(1)[0] % 26)).join('');
+    const randomStr = (len = 6) => Array.from({ length: len }, () => String.fromCharCode(crypto.randomInt(97, 123))).join('');
     const randomHeader = () => `X-Sess-${crypto.randomBytes(4).toString('hex')}`;
 
     return {
