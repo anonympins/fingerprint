846c5eaa9e78b252fc2291d93fb2200b9fc82bbe
anonympins <anonympins@gmail.com>
2026-09-23 02:47:16 +0200

    Update fingerprint.test.js

--- src/js/tests/fingerprint.test.js
+++ src/js/tests/fingerprint.test.js
@@ -2382,7 +2382,7 @@ describe('Subnet Scoring (Node.js)', () => {
             deviceIds: deviceIds, // count = 20. score += (20-10)*5 = 50
         });
         ({ subnetScore } = await __internal.getSubnetScore(context, 'device-1'));
-            expect(subnetScore).toBe(94.4);
+            expect(subnetScore).toBe(99.4);
     });
 });
 
